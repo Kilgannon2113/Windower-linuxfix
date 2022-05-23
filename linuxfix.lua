8ddbee0216d44e908604784c61494ef9e695cc1d
Kilgannon2113 <kievkillgo@gmail.com>
2022-05-23 18:38:20 -0500

    Update linuxfix.lua

--- linuxfix.lua
+++ linuxfix.lua
@@ -1,5 +1,5 @@
 _addon.name    = 'linuxfix'
-_addon.version = '0.4.4'
+_addon.version = '0.5.0'
 _addon.author = 'Rubenator,Surik,BeYoNDLiFe'
 
 --[[
@@ -28,7 +28,7 @@ check_rate = 1
 
 require('luau')
 mods = S{}
-keybinds = T{}
+keybinds = {}
 last_state = T{}
 local dik_lookup = T{[-1]='sysrq', [1]='escape', [2]='1', [3]='2', [4]='3', [5]='4', [6]='5', [7]='6', [8]='7', [9]='8', [10]='9', [11]='0', [12]='-', [13]='=', [14]='backspace', [15]='tab', [16]='q', [17]='w', [18]='e', [19]='r', [20]='t', [21]='y', [22]='u', [23]='i', [24]='o', [25]='p', [26]='[', [27]=']', [28]='enter', [30]='a', [31]='s', [32]='d', [33]='f', [34]='g', [35]='h', [36]='j', [37]='k', [38]='l', [39]="\\;", [40]="'", [41]='`',  [43]='\\\\', [44]='z', [45]='x', [46]='c', [47]='v', [48]='b', [49]='n', [50]='m', [51]=',', [52]='.', [53]='/', [54]='rshift', [55]='numpad*', [57]='space', [58]='capslock', [59]='f1', [60]='f2', [61]='f3', [62]='f4', [63]='f5', [64]='f6', [65]='f7', [66]='f8', [67]='f9', [68]='f10', [69]='numlock', [70]='scrolllock', [71]='numpad7', [72]='numpad8', [73]='numpad9', [74]='numpad-', [75]='numpad4', [76]='numpad5', [77]='numpad6', [78]='numpad+', [79]='numpad1', [80]='numpad2', [81]='numpad3', [82]='numpad0', [83]='numpad.', [86]='oem_102', [87]='f11', [88]='f12', [100]='f13', [101]='f14', [102]='f15', [112]='kana', [115]='abnt_c1', [121]='convert', [123]='noconvert', [125]='yen', [126]='abnt_c2', [141]='numpadequals', [145]='at', [146]='colon', [147]='underline', [148]='kanji', [149]='stop', [150]='ax', [151]='unlabeled', [153]='nexttrack', [156]='numpadenter', [157]='rctrl', [160]='mute', [161]='calculator', [162]='playpause', [164]='mediastop', [174]='volumedown', [176]='volumeup', [178]='webhome', [179]='numpadcomma', [181]='numpad/', [183]='sysrq', [184]='rmenu', [197]='pause', [199]='home', [200]='up', [201]='pageup', [203]='left', [205]='right', [207]='end', [208]='down', [209]='pagedown', [210]='insert', [211]='delete', [220]='rwin', [222]='power', [223]='sleep', [227]='wake', [229]='websearch', [230]='webfavorites', [231]='webrefresh', [232]='webstop', [233]='', [234]='webback', [235]='mycomputer', [236]='mail', [237]='mediaselect'}
 local mod_lookup = T{[29]='Ctrl', [42]='Shift', [56]='Alt', [219]='Win', [221]='Apps'}
@@ -71,17 +71,20 @@ windower.register_event('keyboard',function(dik,pressed)
 end)
 
 function check_bind(key_data)
+	local binds = keybinds[key_data.name]
+	if not binds then return false end
 	local info = windower.ffxi.get_info()
 	local chat_open = info and info.chat_open
-	return check_bind_table(key_data, keybinds) or check_bind_table(key_data, keybinds[chat_open and 'ChatOnly' or 'NoChat'])
-end
-
-function check_bind_table(key_data, t)
-	if mods:length() == 1 then
-		t = t and t[mods:it()()]
+	--if chat_open == nil then return false end
+	binds = binds:filter(function(bind)
+		return (bind.chat_state == nil or chat_open and bind.chat_state == 'ChatOnly' or not chat_open) and (bind.mods:length() == bind.mods:intersection(mods):length())
+	end)
+	local number_of_binds = binds:length()
+	if number_of_binds == 0 then
+		return false
+	else
+		return binds:first().command
 	end
-	t = t and t[key_data.name]
-	return type(t) == 'string' and t or nil
 end
 
 function send_key_state(key_data, pressed)
@@ -93,23 +96,29 @@ windower.register_event('incoming text', function(original, modified, color, col
 	if ListBinds then
 		if original == '===== Done Listing Currently Bound Keys =====' then
 			ListBinds = false
+			for _,binds in pairs(new_keybinds) do
+				binds:sort(function(a,b) return a.mods:length() > b.mods:length() end)
+			end
+			keybinds = new_keybinds
 		else
 			add_bind(original)
 		end
+		return true
 	elseif original == '===== Listing Currently Bound Keys =====' then
 		last_update = os.clock()
-		keybinds = T{}
-		ListBinds = true --The listbinds text doesn't have any unique identifying features so I added this to only capture text while listbinds is running.
+		new_keybinds = {}
+		ListBinds = true
+		return true
 	end
-	return true
 end)
 
-local chat_states = T{['(NoChat)']='NoChat', ['(ChatOnly)']='ChatOnly'}
+local chat_states = {['(NoChat)']='NoChat', ['(ChatOnly)']='ChatOnly'}
 function add_bind(original)
-	local parts = {original:match('^([^%-:]*)%-?([^: ]*): (.*)$')}
-	local command = table.last(parts)
-	table.remove(parts)
-	local chat_state, mod, key
+	local bind, command = original:match('^([^:]*): (.*)$')
+	bind = bind:gsub('^%-', 'minus'):gsub('%-%-', '-minus')
+	parts = bind:split('-'):map(string.gsub-{'minus', '-'})
+	local chat_state, key
+	local mods = S{}
 	for _,part in pairs(parts) do
 		if chat_states[part] then
 			chat_state = chat_states[part]
@@ -117,29 +126,17 @@ function add_bind(original)
 			local key_data = name_lookup[part]
 			if key_data then
 				if key_data.mod then
-					mod = part
+					mods:add(part)
 				else
 					key = part
 				end
 			end
 		end
 	end
-	if key then
-		local t = keybinds
-		if chat_state then
-			if not t[chat_state] then
-				t[chat_state] = {}
-			end
-			t = t[chat_state]
-		end
-		if mod then
-			if not t[mod] then
-				t[mod] = {}
-			end
-			t = t[mod]
-		end
-		t[key] = command
+	if not new_keybinds[key] then
+		new_keybinds[key] = L{}
 	end
+	new_keybinds[key]:append({key=key, mods=mods, chat_state=chat_state, command=command})
 end
 
 windower.send_command('listbinds')
